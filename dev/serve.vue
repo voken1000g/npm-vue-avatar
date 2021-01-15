e28f028244f17a7fbaf9b3f1104dc1658425c05a
Lyman Snow <pmvoken@protonmail.com>
2021-01-15 20:35:39 +0000

    v1.1.0

---
+++ dev/serve.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" style="background-color: bisque; padding: 100px">
+    <div style="width: 64px; height: 64px; background-color: white">
+      <vue-avatar value="sample"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueAvatar from '@/vue-avatar.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueAvatar
+  }
+});
+</script>
+
---
+++ src/vue-avatar.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="dataUri" :alt="'avatar: ' + value">
+</template>
+
+<script>
+import avatar from '@voken/avatar'
+
+export default {
+  name: 'VueAvatar',
+  props: {
+    value: String,
+  },
+  computed: {
+    dataUri() {
+      return avatar.svgDataUriFromSeed(this.value)
+    }
+  }
+}
+</script>
